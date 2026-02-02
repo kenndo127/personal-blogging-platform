c670e9f63624f529cea2e3a882aee8aace52a5f9
Kenneth Okechukwu <kennethchidiebube5@gmail.com>
2026-02-02 07:24:12 +0100

    Search Page fully functional.

--- news.php
+++ news.php
@@ -5,7 +5,7 @@
   }
 
   include("./includes/db_connect.php");
-  include_once("functions.php");
+  include_once("./includes/functions.php");
 
   $slug = $_GET['title']; 
 
@@ -54,33 +54,7 @@
 <body>
 
   <!-- Navbar Section -->
-  <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="index.php"><img src="./assets/logo.png" alt="Logo"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="blog.php">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="newsletter.php">Newsletter</a>
-          </li>
-        </ul>
-
-        <div class="search">
-          <a class="nav-link" href="search.php"><img src="./assets/search.svg" alt="Search Logo"></a>
-        </div>
-
-      </div>
-    </div>
-  </nav>
+  <?php include("./includes/navbar.php"); ?>
 
   <!-- News Section -->
   <main class="news">
--- search.php
+++ search.php
@@ -1,56 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Search</title>
-  <link rel="icon" type="image/x-icon" href="./assets/logo.png">
-
-  <!-- Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Anta&family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Caveat:wght@400..700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Micro+5&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-
-  <!-- CSS -->
-  <link rel="stylesheet" href="./styles/style.css">
-
-</head>
+<!-- Header Goes Here -->
+<?php 
+  $page_title = "Search";
+  include("./includes/header.php") 
+?>
 
 <body>
-
   <!-- Navbar Section -->
-  <div class="trending"><a href="#">This is the latest trending news</a></div>
-  <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="index.php"><img src="./assets/logo.png" alt="Logo"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="blog.php">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="newsletter.php">Newsletter</a>
-          </li>
-        </ul>
-
-        <div class="search">
-          <a class="nav-link" href="search.php"><img src="./assets/search.svg" alt="Search Logo"></a>
-        </div>
-
-      </div>
-    </div>
-  </nav>
+  <?php include("./includes/navbar.php"); ?>
 
   <!-- Search section -->
   <div class="search-page">
@@ -58,28 +14,56 @@
       <div class="row">
         <h2>Search here</h2>
 
-        <form>
+        <form action="search.php" method="get">
           <div>
             <input type="text" class="search-input" name="search" required>
           </div>
           <div>
-            <button type="submit" value="Search" name="search">Search</button>
+            <button type="submit">Search</button>
           </div>
         </form>
+
+        <?php
+          if(isset($_GET['search']) && !empty(trim($_GET['search']))){
+            $key_word = trim($_GET['search'], " ");
+            $search_key_word = "%" . $key_word . "%";
+
+            include_once("./includes/db_connect.php");
+
+            $sql = "SELECT * FROM posts WHERE title LIKE ? OR content LIKE ?";
+            $stmt = mysqli_prepare($connection, $sql);
+            mysqli_stmt_bind_param($stmt, "ss", $search_key_word, $search_key_word);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+
+            if(mysqli_num_rows($result) > 0){
+              echo " <ol class='list-group'>
+                    <h4 class='list-group-item'> Search Results </h4>
+                ";
+              while($row = mysqli_fetch_assoc($result)){
+                
+                $title = htmlspecialchars($row['title']);
+                $slug = urlencode($row['slug']);
+                echo
+                "
+                  <li class='list-group-item'><a style='text-decoration: none;' href='news.php?title={$slug}'>{$title}</a></li>
+                ";
+              }
+              echo "</ol>";
+            } else {
+              echo "No entries found!";
+            }
+          } else {
+            echo "Enter a valid key word";
+          }
+        ?>
+
 
       </div>
     </div>
   </div>
   <!-- Footer Section -->
-  <footer>
-    <p>&copy; Okechukwu Kenneth Chidiebube 2026 </p>
-  </footer>
+  <?php include("./includes/footer.php"); ?>
 
-
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  <script src="index.js"></script>
 </body>
-
 </html>
